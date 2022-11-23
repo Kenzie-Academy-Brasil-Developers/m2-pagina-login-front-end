cb2bde112d6d404330e6db40e52d9af360e661df
caio <chss.bolsa99@gmail.com>
2022-11-23 13:21:22 -0300

    entrega login

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,14 @@
-/* Desenvolva seu código aqui... */
+const botaomodal = document.querySelector("[data-modal-control]")
+
+    botaomodal.addEventListener("click",function(){
+    let valormodal = botaomodal.getAttribute("data-modal-control")
+    document.getElementById(valormodal).classList.toggle("show-modal")
+    console.log(document.getElementById(valormodal))
+
+    })
+
+const botaoremover = document.querySelector(".modalclose")
+botaoremover.addEventListener("click",function(){
+    let valorbotao = botaoremover.getAttribute("data-modal-control")
+    document.getElementById(valorbotao).classList.toggle("show-modal")
+})
